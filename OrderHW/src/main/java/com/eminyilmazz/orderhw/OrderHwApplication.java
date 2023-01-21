9cd370d78cf960ada75a56d1e14d16fb5487aaad
Emin <eminyilmzz@gmail.com>
2023-01-22 02:07:12 +0300

    HW2 Initial

---
+++ OrderHW/src/main/java/com/eminyilmazz/orderhw/OrderHwApplication.java
@@ -0,0 +1,13 @@
+package com.eminyilmazz.orderhw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderHwApplication.class, args);
+    }
+
+}
---
+++ OrderHW/src/test/java/com/eminyilmazz/orderhw/OrderHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eminyilmazz.orderhw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderHwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
