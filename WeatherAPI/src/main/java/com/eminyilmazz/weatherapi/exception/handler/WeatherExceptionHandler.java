f2edcb5dfd9ea2a01c22fe24bc1bfa082f1563a7
Emin <eminyilmzz@gmail.com>
2023-02-01 00:39:08 +0300

    Error logging

--- WeatherAPI/src/main/java/com/eminyilmazz/weatherapi/exception/handler/WeatherExceptionHandler.java
+++ WeatherAPI/src/main/java/com/eminyilmazz/weatherapi/exception/handler/WeatherExceptionHandler.java
@@ -2,6 +2,10 @@ package com.eminyilmazz.weatherapi.exception.handler;
 
 import com.eminyilmazz.weatherapi.exception.CityNotFound;
 import com.eminyilmazz.weatherapi.exception.InvalidAPIKeyException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -13,11 +17,18 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class WeatherExceptionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(WeatherExceptionHandler.class);
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     @ExceptionHandler({InvalidAPIKeyException.class})
     private ResponseEntity<Map<String, String>> handleAPIException(InvalidAPIKeyException e) {
         Map<String, String> error = new HashMap<>();
         error.put("message", e.getMessage());
+        try {
+            logger.error("Error occurred during the request: {}", objectMapper.writeValueAsString(error));
+        } catch (JsonProcessingException ex) {
+            throw new RuntimeException(ex);
+        }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
@@ -25,6 +36,11 @@ public class WeatherExceptionHandler {
     private ResponseEntity<Map<String, String>> handCityNotFoundException(CityNotFound e) {
         Map<String, String> error = new HashMap<>();
         error.put("message", e.getMessage());
+        try {
+            logger.error("Error occurred during the request: {}", objectMapper.writeValueAsString(error));
+        } catch (JsonProcessingException ex) {
+            throw new RuntimeException(ex);
+        }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 
@@ -32,6 +48,11 @@ public class WeatherExceptionHandler {
     private ResponseEntity<Map<String, String>> handleExcetion(Exception e) {
         Map<String, String> error = new HashMap<>();
         error.put("message", e.getMessage());
+        try {
+            logger.error("Error occurred during the request: {}", objectMapper.writeValueAsString(error));
+        } catch (JsonProcessingException ex) {
+            throw new RuntimeException(ex);
+        }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
